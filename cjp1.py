a26ed56cba16070be142f7587ee805f62e349901
Adan2521 <afranc28@ucsc.edu>
2025-10-02 12:08:46 -0700

    Coding Journal Prompt 1

---
+++ cjp1.py
@@ -0,0 +1,11 @@
+def PrintHelloWorld(x):
+	print(f"Full name: Adan Franco Casillas")
+	print(f'Pronouns (optional): He/him')
+	print(f'Favorite moive: The Batman')
+	print(f'Favortie food: Shrimp cocktail')
+
+def main():
+	PrintHelloWorld()
+
+if __name__ == '__main__':
+	main()
