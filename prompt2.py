054c2b873cf2b7bc1b5b20a44ce928b62d800762
Adan2521 <afranc28@ucsc.edu>
2025-10-15 18:04:11 -0700

    cjprompt2

---
+++ prompt2.py
@@ -0,0 +1,20 @@
+x = float(4.0)
+y = float(6.5)
+z = x + y 
+
+print(z)
+print(type(z))
+
+q = int(5)
+p = int(7)
+j = p - q
+
+print(j)
+print(type(j))
+
+w = int(5)
+t = float(6.7)
+g = w * t
+
+print(g)
+print(type(g))
